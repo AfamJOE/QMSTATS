6d46755b20b8edd0151c5c6e72e6f68d7ec2532b
AfamJOE <afam_jo@yahoo.com>
2026-04-25 13:35:14 -0400

    Add database port support

--- backend/db.js
+++ backend/db.js
@@ -4,11 +4,12 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306, 
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
 });
 
 module.exports = pool;
